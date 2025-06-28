924b2f3ccc6d17a0ba712c37fdfb72d11a4905e4
wolftxt <wolfdavid49@gmail.com>
2025-06-28 14:18:19 +0200

    Added all pieces to the PieceFactory

--- src/main/java/tetris/PieceFactory.java
+++ src/main/java/tetris/PieceFactory.java
@@ -3,23 +3,37 @@ package tetris;
 public class PieceFactory {
 
     public static Piece createSquarePiece() {
-        boolean[][] shape = {{true, true}, {true, true}};
+        int[][] shape = {{0, 0}, {0, 0}};
         return new Piece(shape);
     }
 
     public static Piece createLPiece() {
-        boolean[][] shape = {{true, false, false}, {true, true, true}, {false, false, false}};
+        int[][] shape = {{1, -1, -1}, {1, 1, 1}, {-1, -1, -1}};
         return new Piece(shape);
     }
 
     public static Piece createJPiece() {
-        boolean[][] shape = {{false, false, true}, {true, true, true}, {false, false, false}};
+        int[][] shape = {{-1, -1, 2}, {2, 2, 2}, {-1, -1, -1}};
         return new Piece(shape);
     }
 
     public static Piece createTPiece() {
-        boolean[][] shape = {{false, true, false}, {true, true, true}, {false, false, false}};
+        int[][] shape = {{-1, 3, -1}, {3, 3, 3}, {-1, -1, -1}};
         return new Piece(shape);
     }
 
+    public static Piece createSPiece() {
+        int[][] shape = {{-1, 4, 4}, {4, 4, -1}, {-1, -1, -1}};
+        return new Piece(shape);
+    }
+
+    public static Piece createZPiece() {
+        int[][] shape = {{5, 5, -1}, {-1, 5, 5}, {-1, -1, -1}};
+        return new Piece(shape);
+    }
+
+    public static Piece createIPiece() {
+        int[][] shape = {{-1, -1, -1, -1}, {6, 6, 6, 6}, {-1, -1, -1, -1}, {-1, -1, -1, -1}};
+        return new Piece(shape);
+    }
 }
