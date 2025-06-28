89be9764274cad8192756764069b7a33bf3af86a
wolftxt <wolfdavid49@gmail.com>
2025-06-28 14:25:38 +0200

    Implemented rotate function

--- src/main/java/tetris/Piece.java
+++ src/main/java/tetris/Piece.java
@@ -2,9 +2,28 @@ package tetris;
 
 public class Piece {
 
-    private boolean[][] shape;
+    private int[][] shape;
 
-    public Piece(boolean[][] shape) {
+    public Piece(int[][] shape) {
+        if (shape.length != shape[0].length) {
+            throw new RuntimeException("Shape array must be a square matrix");
+        }
         this.shape = shape;
+    }
+    
+    public void rotate(){
+        for (int x = 0; x < shape.length / 2; x++) {
+            int ndIndex = shape.length - x - 1;
+            int[] temp = shape[x];
+            shape[x] = shape[ndIndex];
+            shape[ndIndex] = temp;
+        }
+        for (int x = 0; x < shape.length; x++) {
+            for (int y = 0; y < x; y++) {
+                int temp = shape[x][y];
+                shape[x][y] = shape[y][x];
+                shape[y][x] = temp;
+            }
+        }
     }
 }
