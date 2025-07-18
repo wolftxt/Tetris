943e065bb60501b3c100d78e3c7fc2da1b750121
wolftxt <wolfdavid49@gmail.com>
2025-07-18 14:00:20 +0200

    Added game screenshots

--- src/main/java/tetris/view/Popups.java
+++ src/main/java/tetris/view/Popups.java
@@ -5,7 +5,6 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.util.Arrays;
 import java.util.Scanner;
 import javax.swing.*;
 import tetris.settings.ControllsSettings;
